6803526e791c804d37b6f676f92c32e571b8939d
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-18 08:39:13 -0300

    un detalle, nada importante

--- src/components/pages/peliculas/Pelicula.jsx
+++ src/components/pages/peliculas/Pelicula.jsx
@@ -8,48 +8,48 @@ import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Button, IconButton } from '@mui/material';
 
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
-const Pelicula = ({movie, handleLike, deleteMovieById}) => {
+const Pelicula = ({ movie, handleLike, deleteMovieById }) => {
 
-    let {name, genre, description, createdAt, img, isLiked} = movie
- 
-    // rutas dinamicas
+  let { name, genre, description, createdAt, img, isLiked } = movie
+
+  // rutas dinamicas
   /* let {id} = useParams()
   let peliSeleccionada = movie.find(peli => peli.id === parseInt(id)) */
 
-    return (
+  return (
 
-        <Card sx={{ width: 300, height: 650 }}>
+    <Card sx={{ width: 300, height: 650 }}>
       <CardHeader
-                
+
         title={name}
         subheader={createdAt}
       />
       <CardMedia
         component="img"
-        height="194"        
+        height="194"
         image={img}
         alt={name}
       />
       <CardContent sx={{ height: 260 }}>
         <Typography variant="body2" color="text.secondary">
           <h4>{genre}</h4>
-            <p> {description} </p>
+          <p> {description} </p>
 
         </Typography>
       </CardContent>
 
-      <CardActions sx={{display:'flex', justifyContent:'space-between'}}>
+      <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <IconButton aria-label="add to favorites">
-          <FavoriteIcon color={ isLiked ? "error" : "disabled"} onClick={()=> handleLike(movie) }/>
+          <FavoriteIcon color={isLiked ? "error" : "disabled"} onClick={() => handleLike(movie)} />
         </IconButton>
-        <Button type='button' variant='contained' color='primary' onClick={()=>deleteMovieById(movie.id)}>Eliminar</Button>
+        <Button type='button' variant='contained' color='primary' onClick={() => deleteMovieById(movie.id)}>Eliminar</Button>
       </CardActions>
-      
+
     </Card>
-       
-    )
+
+  )
 }
 
 export default Pelicula
