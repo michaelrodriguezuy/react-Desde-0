f240f0d96431be87ed9aa95eea8cf7f781a84b4b
michaelrodriguezuy <hola@olimarteam.uy>
2024-03-11 01:05:00 -0300

    modificacion en peticion patch 3

--- frontend/src/components/pages/peliculas/Peliculas.jsx
+++ frontend/src/components/pages/peliculas/Peliculas.jsx
@@ -55,7 +55,7 @@ const Peliculas = () => {
       })
       .then((response) => {
         // Actualiza el estado con el valor actualizado de isLiked de la respuesta
-        console.log("isLiked: ",response.data.isliked);
+        console.log("isLiked: ",response.data);
         setIsLiked(response.data.isliked);
       })
       .catch((error) => console.log(error));
